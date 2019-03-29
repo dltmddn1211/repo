0c1898da4777fc8f587714a066131d5117dd2290
dltmddn1211 <eddie0000yo@gmail.com>
2019-03-29 17:15:53 +0900

    my commit

---
+++ exe1.py
@@ -0,0 +1,14 @@
+# name='Lee'
+# mi=[1,2,3,4]
+# ms=(4,5,)
+# qqq=input()
+# print(f"{name}")
+# print(f"{mi[2]}")
+# print(f"{name},{ms}")
+# print(f"{qqq}") 출력
+
+sum=3000000*0.5
+year=sum*12
+h=year+year*0.024
+
+print("P씨의 정기예금 만기 금액은 {:,}원 입니다".format(int(h)))
